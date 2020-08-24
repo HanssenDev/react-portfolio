4e1448c135942a55fab73c416c17bde8b385d7d9
Hanssen <hramanjooloo@gmail.com>
2020-08-24 21:26:46 +0100

    create about me section with an avatar svg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import Container from "react-bootstrap/container";
+import { Container, Row } from "react-bootstrap/";
+import { ReactComponent as ReactLogo } from "../assets/avatar.svg";
 
 function About() {
   return (
     <section id="about">
+      <h2>About Me</h2>
       <Container>
-        <h2>About</h2>
+        <Row md={2}>
+          <div className="avatar-box">
+            <ReactLogo />
+          </div>
+          <div className="about-box">
+            <h4>Front-End Developer</h4>
+            <p>
+              Hi there! I'm <span className="name">Hanssen</span> and I'm a
+              Self-Taught Front-End Developer based in London, UK.
+            </p>
+            <p>
+              I decided to learn coding as a challenge and have thoroughly
+              enjoyed it so far. The challenge that it presents keeps me engaged
+              and it is now a hobby of mine to learn new technologies.
+            </p>
+          </div>
+        </Row>
       </Container>
     </section>
   );

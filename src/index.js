3206d682fe81b3b37004414bb61ecac26e587557
Hanssen <hramanjooloo@gmail.com>
2020-08-30 00:14:41 +0100

    refactor css into sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./app.css";
+import "./css/app.css";
 
 ReactDOM.render(
   <React.StrictMode>
